6fe189458e6aa716f7d4235cceed184088d51b42
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-03 00:22:16 -0700

    fix merge conflict

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4242").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(_) => println!("connection accepted"),
