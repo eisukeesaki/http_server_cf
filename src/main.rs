0a2b2853a17667a6fb2dec22698c36409aeedd25
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-03 00:08:57 -0700

    accept tcp connections. Eisuke's solution.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
+    let listener = TcpListener::bind("127.0.0.1:4242").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Ok(_) => println!("connection accepted"),
+            Err(e) => println!("failed to connect. {}", e),
         }
     }
 }
+
