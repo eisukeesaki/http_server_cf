de61175baa03d5264167f4c99668fa5cef064d6a
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-03 15:59:00 -0700

    respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Write, Error};
+
+// fn getSockAddr(stream: TcpStram) -> Result<, Error> {
+//
+// }
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let addr = match stream.peer_addr() {
+        Ok(addr) => addr,
+        Err(e) => {
+            println!("Failed to get address from TcpStream. {}", e);
+            return Err(e);
+        }
+    };
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => {
+            println!("Written `{}` to {:?}", response, addr);
+            Ok(())
+        },
+        Err(e) => {
+            println!("Failed to write to {:?}.", addr);
+            Err(e)
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_) => println!("connection accepted"),
-            Err(e) => println!("failed to connect. {}", e),
+            Ok(stream) => {
+                println!("Accepted connection request from {:?}",
+                    stream.peer_addr().unwrap());
+                match handle_connection(stream) {
+                    Ok(_) => println!("Response sent."),
+                    Err(e) =>  println!("Failed to send response. {}", e),
+                };
+            },
+            Err(e) => println!("Failed to connect. {}", e),
         }
     }
 }
