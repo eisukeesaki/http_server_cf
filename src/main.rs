7a33ce0c19853d7ddf941d245bd7d990a3e22ea0
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-03 23:33:50 -0700

    refactor. respond with 200

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,33 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Error};
 
-// fn getSockAddr(stream: TcpStram) -> Result<, Error> {
-//
-// }
-
-fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    let addr = match stream.peer_addr() {
-        Ok(addr) => addr,
-        Err(e) => {
-            println!("Failed to get address from TcpStream. {}", e);
-            return Err(e);
-        }
-    };
-    match stream.write_all(response.as_bytes()) {
-        Ok(_) => {
-            println!("Written `{}` to {:?}", response, addr);
-            Ok(())
+fn main() {
+    let socket = "127.0.0.1:4221";
+    let listener = match TcpListener::bind(socket) {
+        Ok(l) => {
+            println!("Server bound to {}", socket);
+            l
         },
         Err(e) => {
-            println!("Failed to write to {:?}.", addr);
-            Err(e)
+            eprintln!("Could not bind to {}. {}", socket, e);
+            std::process::exit(1);
         }
-    }
-}
+    };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("Accepted connection request from {:?}",
-                    stream.peer_addr().unwrap());
-                match handle_connection(stream) {
-                    Ok(_) => println!("Response sent."),
-                    Err(e) =>  println!("Failed to send response. {}", e),
-                };
-            },
-            Err(e) => println!("Failed to connect. {}", e),
+    match listener.accept() {
+        Ok((mut stream, socket)) => {
+            println!("Accepted connection request from {}", socket);
+            let status = "HTTP/1.1 200 OK\r\n\r\n";
+            match stream.write(status.as_bytes()) {
+                Ok(size_written) => println!("{} bytes written to {}",
+                    size_written, socket),
+                Err(e) => eprintln!("Failed to write to {} {}", socket, e)
+            };
+        },
+        Err(e) => {
+            eprintln!("Failed to accept connection from {} {}", socket, e);
+            std::process::exit(1);
         }
-    }
+    };
 }
 
