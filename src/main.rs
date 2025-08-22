b0be18fe104b3888c10c6024e4ecda50b236ce42
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-21 23:01:44 -0700

    handle client in a dedicated function

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
 #[allow(unused_imports)]
 use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Error};
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    let status = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = status.as_bytes();
+    match stream.write(response) {
+        Ok(size_written) => println!("{} bytes written", size_written),
+        Err(e) => eprintln!("Failed to write to {}", e)
+    }
+}
 
 fn main() {
     let socket = "127.0.0.1:4221";
     let listener = match TcpListener::bind(socket) {
-        Ok(l) => {
-            println!("Server bound to {}", socket);
-            l
-        },
+        Ok(listener) => listener,
         Err(e) => {
-            eprintln!("Could not bind to {}. {}", socket, e);
+            eprintln!("Could not bind to socket. {}", e);
             std::process::exit(1);
         }
     };
 
     match listener.accept() {
-        Ok((mut stream, socket)) => {
-            println!("Accepted connection request from {}", socket);
-            let status = "HTTP/1.1 200 OK\r\n\r\n";
-            match stream.write(status.as_bytes()) {
-                Ok(size_written) => println!("{} bytes written to {}",
-                    size_written, socket),
-                Err(e) => eprintln!("Failed to write to {} {}", socket, e)
-            };
-        },
+        Ok((stream, _)) => handle_client(stream),
         Err(e) => {
-            eprintln!("Failed to accept connection from {} {}", socket, e);
+            eprintln!("Failed to accept connection. {}", e);
             std::process::exit(1);
         }
-    };
+    }
 }
 
